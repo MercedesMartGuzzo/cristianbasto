bba193a18fd38bb30529a80b1f4220d01b73bff1
Mercedes Martinez <mercedesviolin@gmail.com>
2024-06-10 22:43:32 -0300

    Agregué la sección agenda y su archivo js

---
+++ js/agenda.js
@@ -0,0 +1,84 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const eventosLista = document.getElementById('eventos');
+
+    // Lista de eventos a ser actualizada por el programador
+    const eventos = [
+        {
+            name: "Quasimodo Trío y Cristian Basto Ensamble",
+            date: "26-06-2024",
+            time: "20:00 hs",
+            location: "La Tierra Invisible",
+         /*    imageUrl: "./img/laTierraInvisible.jpg", */
+            mapUrl: "https:///maps.app.goo.gl/sUn7cwrhY9UWFiiw9"
+        },
+        {
+            name: "Feria del Libro",
+            date: "2024-07-10",
+            time: "10:00",
+            location: "Centro de Convenciones",
+            imageUrl: "path/to/centro-convenciones.jpg",
+            mapUrl: "https://www.google.com/maps?q=Centro+de+Convenciones"
+        },
+        {
+            name: "Conferencia de Tecnología",
+            date: "2024-08-15",
+            time: "09:00",
+            location: "Hotel Hilton",
+            imageUrl: "path/to/hotel-hilton.jpg",
+            mapUrl: "https://www.google.com/maps?q=Hotel+Hilton"
+        },
+        {
+            name: "Festival de Cine",
+            date: "2024-09-05",
+            time: "20:00",
+            location: "Parque Central",
+            imageUrl: "path/to/parque-central.jpg",
+            mapUrl: "https://www.google.com/maps?q=Parque+Central"
+        }
+    ];
+
+    // Función para agregar un evento a la lista
+    function addEvent(name, date, time, location, imageUrl, mapUrl) {
+        const eventItem = document.createElement('li');
+
+        eventItem.innerHTML = `
+            <div>
+            <span>${name} </span>
+            </div>
+             <div>
+            <span>${date}</span> 
+             </div>
+             <div>
+            <span>${time} </span>  
+            </div>
+            <div>
+            <span> ${location} </span> 
+            </div>
+            <button class="view-more-btn">Ver más</button>
+            <div class="event-details" style="display: none;">
+                <img src="${imageUrl}" alt="Imagen de ${location}" class="location-image">
+                <a href="${mapUrl}" target="_blank" class="location-map">Ver en el mapa</a>
+            </div>
+        `;
+
+        const viewMoreBtn = eventItem.querySelector('.view-more-btn');
+        const eventDetails = eventItem.querySelector('.event-details');
+
+        viewMoreBtn.addEventListener('click', () => {
+            if (eventDetails.style.display === "none") {
+                eventDetails.style.display = "block";
+                viewMoreBtn.textContent = "Ver menos";
+            } else {
+                eventDetails.style.display = "none";
+                viewMoreBtn.textContent = "Ver más";
+            }
+        });
+
+        eventosLista.appendChild(eventItem);
+    }
+
+    // Recorrer y agregar cada evento de la lista al DOM
+    eventos.forEach(event => {
+        addEvent(event.name, event.date, event.time, event.location, event.imageUrl, event.mapUrl);
+    });
+});
--- js/index.js
+++ js/index.js
@@ -1,66 +1,68 @@
 // Agregar un evento de clic a todos los enlaces del menú
+ 
 document.querySelectorAll(".navbar-nav .nav-link").forEach((item) => {
-  // Agregar un listener de clic a cada enlace
   item.addEventListener("click", () => {
-    // Obtener el botón de la barra de navegación
+   
     let navbarToggler = document.querySelector(".navbar-toggler");
-    // Si el botón de la barra de navegación está presente y está expandido (es decir, el menú está abierto)
     if (navbarToggler && !navbarToggler.classList.contains("collapsed")) {
-      // Hacer clic en el botón para cerrar el menú
       navbarToggler.click();
     }
   });
 });
 
+// Event listener solo para los enlaces dentro del div "redes"
+document.querySelectorAll('.redes a').forEach(item => {
+  item.addEventListener('click', event => {
+      event.preventDefault(); 
+      const url = item.getAttribute('href'); 
+      if (url) {
+          window.open(url, '_blank');
+      }
+  });
+});
+
+
+
 // Definir una función para activar/desactivar los bordes de una sección
 function activarDesactivarBordes(contenedor) {
   // Obtener las dimensiones y la posición del contenedor con respecto al viewport
   let rect = contenedor.getBoundingClientRect();
-
-  // Aparecer borde superior cuando el borde superior del contenedor está dentro del viewport
   if (rect.top <= window.innerHeight) {
-    // Agregar la clase 'border-visible-top' al cont`videosConenedor para mostrar el borde superior
     contenedor.classList.add("border-visible-top");
-    // Imprimir un mensaje en la consola indicando que el borde superior ha sido activado
+
     console.log(`Borde superior de ${videosContainer} activado`);
   } else {
-    // Si el borde superior del contenedor no está dentro del viewport, quitar la clase 'border-visible-top'
     contenedor.classList.remove("border-visible-top");
-    // Imprimir un mensaje en la consola indicando que el borde superior ha sido desactivado
+  
     console.log(`Borde superior de ${videosContainer} desactivado`);
   }
 
   // Aparecer borde izquierdo cuando el borde superior del contenedor está dentro del viewport
   if (rect.top <= window.innerHeight) {
-    // Agregar la clase 'border-visible-left' al contenedor para mostrar el borde izquierdo
     contenedor.classList.add("border-visible-left");
-    // Imprimir un mensaje en la consola indicando que el borde izquierdo ha sido activado
     console.log(`Borde izquierdo de ${videosContainer} activado`);
   } else {
-    // Si el borde superior del contenedor no está dentro del viewport, quitar la clase 'border-visible-left'
     contenedor.classList.remove("border-visible-left");
-    // Imprimir un mensaje en la consola indicando que el borde izquierdo ha sido desactivado
     console.log(`Borde izquierdo de ${videosContainer} desactivado`);
   }
 }
 window.addEventListener("scroll", function () {
-  // Obtener el contenedor de videos por su ID
+
   let videosContainer = document.getElementById("videosContainer");
-  // Llamar a la función para el contenedor de videos
   activarDesactivarBordes(videosContainer);
 
-  // Obtener el contenedor de la bio por su ID
+
   let bioContainer = document.getElementById("bioContainer");
-  // Llamar a la función para el contenedor de la bio
   activarDesactivarBordes(bioContainer);
+
+  let agendaContainer =this.document.getElementById("agendaContainer");
+  activarDesactivarBordes(agendaContainer);
 });
 
 function activarBordesMusicaContainer(contenedor) {
-  // Agregar la clase 'border-visible-top' al contenedor para mostrar el borde superior
   contenedor.classList.add("border-visible-top");
-  // Agregar la clase 'border-visible-left' al contenedor para mostrar el borde izquierdo
   contenedor.classList.add("border-visible-left");
-  // Imprimir un mensaje en la consola indicando que los bordes han sido activados
+
   console.log(`Bordes de ${contenedor.id} activados`);
 }
 let musicaContainer = document.getElementById("musicaContainer");
@@ -74,7 +76,7 @@ function smoothScroll(target) {
 
   // Usar window.scrollTo() para agregar desplazamiento suave
   window.scrollTo({
-    top: targetPosition - navbarHeight, // Ajustar el desplazamiento para tener en cuenta la altura de la barra de navegación
+    top: targetPosition - navbarHeight,
     behavior: "smooth",
   });
 }
@@ -83,13 +85,9 @@ function smoothScroll(target) {
 let navLinks = document.querySelectorAll(".navbar-nav a");
 navLinks.forEach(function (navLink) {
   navLink.addEventListener("click", function (event) {
-    // Prevenir el comportamiento predeterminado del enlace
     event.preventDefault();
 
-    // Obtener el hash del atributo href
     let target = this.getAttribute("href");
-
-    // Llamar a la función smoothScroll() con el hash como argumento
     smoothScroll(target);
   });
 });
