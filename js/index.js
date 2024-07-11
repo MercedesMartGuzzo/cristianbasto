54b17cebea8b2270d05cf9ae578d4b177620d652
Mercedes Martinez <mercedesviolin@gmail.com>
2024-07-11 14:10:03 -0300

    Agregue el  archivo pedagogia.js y el estilo dark-mode al  sitio

--- js/index.js
+++ js/index.js
@@ -99,7 +99,7 @@ navLinks.forEach(function (navLink) {
 
 
 /* ICONOS PARA MOSTRAR PARRAFOS DE BIO */
- document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
   const toggleIcons = document.querySelectorAll('.toggle-icon');
   const paragraphs = document.querySelectorAll('.bio-parrafo');
 
@@ -118,7 +118,44 @@ navLinks.forEach(function (navLink) {
       });
     });
   });
-}); 
+});
+
+
+//ACTIVAR-DESACTIVAR DARK-MODE//
+document.addEventListener("DOMContentLoaded", () => {
+  const botonColorMode = document.querySelector("#color-mode");
+  const body = document.body;
+
+  let darkMode = localStorage.getItem("dark-mode");
+
+  function activarDarkMode() {
+      body.classList.add("dark-mode");
+      botonColorMode.classList.replace("bi-moon-stars-fill", "bi-sun-fill");
+      localStorage.setItem("dark-mode", "activado");
+  }
+
+  function desactivarDarkMode() {
+      body.classList.remove("dark-mode");
+      botonColorMode.classList.replace("bi-sun-fill", "bi-moon-stars-fill");
+      localStorage.setItem("dark-mode", "desactivado");
+  }
+
+  if (darkMode === "activado") {
+      activarDarkMode();
+  } else {
+      desactivarDarkMode();
+  }
+
+  botonColorMode.addEventListener("click", () => {
+      darkMode = localStorage.getItem("dark-mode");
+
+      if (darkMode === "activado") {
+          desactivarDarkMode();
+      } else {
+          activarDarkMode();
+      }
+  });
+});
 
 
 
