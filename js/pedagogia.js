3cdfdd8f6c48fa8ab2c24e381b777a4c3cc2dfcd
Mercedes Martinez <mercedesviolin@gmail.com>
2024-11-21 20:33:20 -0300

    agregue galeria

--- js/pedagogia.js
+++ js/pedagogia.js
@@ -12,41 +12,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-    const botonColorMode = document.querySelector("#color-mode");
-    const body = document.body;
-
-    let darkMode = localStorage.getItem("dark-mode");
-
-    function activarDarkMode() {
-        body.classList.add("dark-mode");
-        botonColorMode.classList.replace("bi-moon-stars-fill", "bi-sun-fill");
-        localStorage.setItem("dark-mode", "activado");
-    }
-
-    function desactivarDarkMode() {
-        body.classList.remove("dark-mode");
-        botonColorMode.classList.replace("bi-sun-fill", "bi-moon-stars-fill");
-        localStorage.setItem("dark-mode", "desactivado");
-    }
-
-    if (darkMode === "activado") {
-        activarDarkMode();
-    } else {
-        desactivarDarkMode();
-    }
-
-    botonColorMode.addEventListener("click", () => {
-        darkMode = localStorage.getItem("dark-mode");
-
-        if (darkMode === "activado") {
-            desactivarDarkMode();
-        } else {
-            activarDarkMode();
-        }
-    });
-});
-
 
 document.addEventListener('DOMContentLoaded', function () {
     // Selecciona todos los elementos que deseas animar
